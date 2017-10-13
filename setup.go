8de11c78ffba0a344eea0c454a2506b0d5499a1d
Dan Abad <dan.r.abad@gmail.com>
2017-10-12 20:39:58 -0500

    Add 'local' and 'gopath' commands to setup command
    These commands will setup the gooey package to be used as a
    local package within another package or as a package to be used
    from the gopath.  By default gooey assumes to be installed on the
    developers gopath.

--- setup.go
+++ setup.go
@@ -5,12 +5,24 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"go/format"
+	"go/parser"
+	"go/token"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
+	locals := []string {
+		`"./filewatch"`,
+		`"./websocket"`,
+	}
+	gopaths := []string {
+		`"github.com/0xABAD/gooey/filewatch"`,
+		`"github.com/0xABAD/gooey/websocket"`,
+	}
+
 	if len(os.Args) >= 2 {
 		switch os.Args[1] {
 		case "favicon":
@@ -20,6 +32,13 @@ func main() {
 				packageName = os.Args[3]
 				favgen(os.Args[2], packageName)
 			}
+
+		case "local":
+			reimport(gopaths, locals)
+
+		case "gopath":
+			reimport(locals, gopaths)
+
 		default:
 			fmt.Println("Unrecognized setup command:", os.Args[1])
 		}
@@ -52,4 +71,35 @@ func favgen(faviconPath, packageName string) {
 
 	str := base64.StdEncoding.EncodeToString(fav)
 	fmt.Fprintf(out, FAVICON_GO, packageName, str)
+}
+
+// Changes the imports in file, gooey.go, to have the import names
+// in the 'from' slice to become the corresponding name in the 'to'
+// slice.  In other words, if an import in gooey.go is equal to
+// 'from[i]' then it will be changed to the value in 'to[i]'.  After
+// the imports are changed then changes are written back to gooey.go.
+func reimport(from, to []string) {
+	if len(from) != len(to) {
+		log.Fatalln("from and to slices in reimport don't have the same length.")
+	}
+
+	fset := token.NewFileSet()
+	parsed, err := parser.ParseFile(fset, "gooey.go", nil, parser.ParseComments)
+	if err != nil {
+		log.Fatalln("Failed to parse gooey.go.  Is setup being run from gooey's package directory? --", err)
+	}
+
+	for _, s := range parsed.Imports {
+		for i, imp := range from {
+			if s.Path.Value == imp {
+				s.Path.Value = to[i]
+			}
+		}
+	}
+
+	f, err := os.Create("gooey.go")
+	if err != nil {
+		log.Fatalln("Could not modify gooey.go to localize imports --", err)
+	}
+	format.Node(f, fset, parsed)
 }
